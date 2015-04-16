acbf1506b5bfc77c12daa6b13dafcbf12b79b8fb
hmyr <darya.pashuto@gmail.com>
2015-04-17 10:18:26 +1100

    tbi

---
+++ dep_correction.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from time import strftime, gmtime
+import codecs, sys, os
+
+
+# tbi
+
+
+
+if __name__ == '__main__':
+    t1 = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
