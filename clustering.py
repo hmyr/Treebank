f2cc00c818ecd4aeed274edbcce46820f80e4a8e
hmyr <darya.pashuto@gmail.com>
2015-04-29 09:23:36 +1100

    structure changes

--- clustering.py
+++ clustering.py
@@ -12,34 +12,49 @@ from sklearn.decomposition import PCA
 class DECluster(object):
     ids = []
 
-    def __init__(self, fn, n_test_samples=1000, target_feat_name='childCheck', id_name='id', separator=','):
+    def __init__(self, fn, n_test_samples=300,  n_clusters=5, target_feat_name='childCheck', id_name='id', separator=','):
         self.target, self.features, self.feat_names, self.feat_ids = DECluster.read_features(fn)
-        self.n_samples, self.n_features = self.features.shape
-        self.n_digits = len(np.unique(self.target))
-        self.sample_size = 300
+        self.n_samples, self.n_features = self.features[0:n_test_samples].shape
         self.target = self.target[0:n_test_samples]
         self.features = self.features[0:n_test_samples]
         self.feat_names = self.feat_names[0:n_test_samples]
         self.feat_ids = self.feat_ids[0:n_test_samples]
-
+        self.n_clusters = n_clusters
+        self.sample_size = 3
 
     @classmethod
-    def read_features(cls, fn,  target_feat_name='childCheck', id_name='id', separator=','):
+    def read_features(cls, fn,  target_feat_name='childCheck', id_name='id', delimiter=';', default_target_value=False):
+        """
+        'id' reads as follows: head.SID, head.WID, child.WID)
+        :param fn:
+        :param target_feat_name:
+        :param id_name:
+        :param separator:
+        :return:
+        """
         feats = []
         target_class = []
         ids = []
         with open(fn, 'rb') as infile:
-            csv_reader = csv.DictReader(infile, delimiter=separator)
+            csv_reader = csv.DictReader(infile, delimiter=delimiter)
             feat_names = csv_reader.fieldnames
-            for row in csv_reader:
-                if row[target_feat_name]:
-                    target_class.append(cls.convert_features(row[target_feat_name]))
-                if row[id_name]:
-                    ids.append(row[id_name])
-                feats.append([cls.convert_features(row[fieldname]) for fieldname in feat_names
-                              if fieldname != target_feat_name and fieldname != id_name])
+            if target_feat_name in feat_names:
+                for row in csv_reader:
+                    if row[target_feat_name]:
+                        target_class.append(cls.convert_features(row[target_feat_name]))
+                    if row[id_name]:
+                        ids.append(row[id_name])
+                    feats.append([cls.convert_features(row[fieldname]) for fieldname in feat_names
+                                 if fieldname != target_feat_name and fieldname != id_name])
+                feat_names.remove(target_feat_name)
+            else:
+                for row in csv_reader:
+                    target_class.append(default_target_value)
+                    if row[id_name]:
+                        ids.append(row[id_name])
+                    feats.append([cls.convert_features(row[fieldname]) for fieldname in feat_names
+                                  if fieldname != target_feat_name and fieldname != id_name])
 
-        feat_names.remove(target_feat_name)
         feat_names.remove(id_name)
         return np.array(target_class, dtype=bool), np.array(feats, dtype=object), feat_names, ids
 
@@ -48,7 +63,6 @@ class DECluster(object):
         if item == 'True': return True
         elif item == 'False': return False
         elif item.isdigit: return int(item)
-
 
     def log_it(self):
         sys.stderr.write(("\nn_targets: %d, \t n_samples %d, \t n_features %d"
@@ -60,34 +74,38 @@ class DECluster(object):
     def bench_k_means(self, estimator, name, data):
         t0 = time.time()
         estimator.fit(data)
-        sys.stderr.write(('\n% 9s   %.2fs    %i   %.3f   %.3f   %.3f   %.3f   %.3f    %.3f'
+        sys.stderr.write(('\n% 9s   %.2fs    %i   %.3f   %.3f   %.3f   %.3f   %.3f'
                           % (name, ( time.time() - t0), estimator.inertia_,
                             metrics.homogeneity_score(self.target, estimator.labels_),
                             metrics.completeness_score(self.target, estimator.labels_),
                             metrics.v_measure_score(self.target, estimator.labels_),
                             metrics.adjusted_rand_score(self.target, estimator.labels_),
-                            metrics.adjusted_mutual_info_score(self.target,  estimator.labels_),
-                            metrics.silhouette_score(data, estimator.labels_,
-                                                     metric='euclidean',
-                                                     sample_size=self.sample_size))))
+                            metrics.adjusted_mutual_info_score(self.target,  estimator.labels_))))
 
-    def clusterize(self, init=None, n_init=10):
+    def evaluate_clust(self, init=None, n_init=10):
         self.log_it()
-        self.bench_k_means(KMeans(init='k-means++', n_clusters=self.n_digits, n_init=10),
+        self.bench_k_means(KMeans(init='k-means++', n_clusters=self.n_clusters, n_init=n_init),
               name="k-means++", data=self.features)
 
-        self.bench_k_means(KMeans(init='random', n_clusters=self.n_digits, n_init=10),
+        self.bench_k_means(KMeans(init='random', n_clusters=self.n_clusters, n_init=n_init),
               name="random", data=self.features)
 
-        pca = PCA(n_components=self.n_digits).fit(self.features)
-        self.bench_k_means(KMeans(init=pca.components_, n_clusters=self.n_digits, n_init=1),
+        pca = PCA(n_components=self.n_clusters).fit(self.features)
+        self.bench_k_means(KMeans(init=pca.components_, n_clusters=self.n_clusters, n_init=1),
               name="PCA-based",
               data=self.features)
-        sys.stderr.write('\n%s' % 79 * '_')
 
+    def clusterize(self, init='k-means++', n_init=5):
         self.reduced_data = PCA(n_components=2).fit_transform(self.features)
-        self.kmeans = KMeans(init='k-means++', n_clusters=self.n_digits, n_init=10)
+        self.kmeans = KMeans(init=init, n_clusters=self.n_clusters, n_init=n_init)
         self.kmeans.fit(self.reduced_data)
+
+    def save_clusters(self, outfn):
+        headers = ['cluster', 'id']
+        with open(outfn, 'wb') as outfile:
+            outfile.write(';'.join(headers) + '\n')
+            outfile.write('\n'.join('%s;%s' % (str(cl), el_id)
+                                    for cl, el_id in zip(self.kmeans.labels_, self.feat_ids)))
 
     def plot_clusters(self):
         h = .02
@@ -119,10 +137,15 @@ class DECluster(object):
 
 if __name__ == '__main__':
     in_fn = sys.argv[1]
+    n_clusters = 3
+    n_test_samples = 45
+    outfn = '/media/darya/Data/LingDir/treebank/clusterized_features_%s_n_clusters_%s_n_test_samples.csv' \
+            % (n_clusters, n_test_samples)
 
-    decluster = DECluster(in_fn)
-    decluster.clusterize()
+    decluster = DECluster(in_fn, n_clusters=n_clusters, n_test_samples=n_test_samples)
+    decluster.clusterize(n_init=2)
     decluster.plot_clusters()
+    decluster.save_clusters(outfn)
 
 
 
